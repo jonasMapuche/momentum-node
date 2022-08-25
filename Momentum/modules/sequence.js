dfc77986d4b191b72ff3ac62c6b9e000b28c931c
jonas mapuche <64318160+jonasMapuche@users.noreply.github.com>
2022-08-25 00:55:15 -0300

    Mudança da conexão para utilização do mongoose e a criação do modelo de persistencia do malware.

--- Momentum/modules/sequence.js
+++ Momentum/modules/sequence.js
@@ -1,4 +1,5 @@
 var mongodb = require('mongodb');
+var mongoose = require('mongoose');
 var mongoClient = mongodb.MongoClient;
 var url = "mongodb+srv://jonas:freedown@cluster0.28oko.azure.mongodb.net/letterDB?retryWrites=true&w=majority"
 var dbName = "letterDB";
@@ -15,6 +16,28 @@ exports.save = (req, res) => {
 };
 
 function save(val) {
+    mongoose.connect(url, { useNewUrlParser: true });
+    const db = mongoose.connection;
+    //db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+    const CompletedSchema = new mongoose.Schema(
+        {
+            type: { type: String, enum: ['course', 'classroom'], required: true },
+            parentslug: { type: String, required: true },
+            slug: { type: String, required: true },
+            userid: { type: String, required: true }
+        },
+        { collection: collectionName }
+    );
+
+    //CompletedSchema.index({ slug: 1, userid: 1 }, { unique: true })
+
+    const model = mongoose.model(dbName, CompletedSchema);
+
+    return val;
+};
+
+function saveMongoDB(val) {
     mongoClient.connect(url, function (err, database) {
         if (err) throw err;
         const db = database.db(dbName);
